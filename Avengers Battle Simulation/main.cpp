9095633b1301f37b6b1283cfd5498156efca54d0
Vignan Srikesh Bathini <bathinivignansrikesh@gmail.com>
2025-05-20 16:48:02 +0530

    Add files via upload

---
+++ Avengers Battle Simulation/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cassert>
+#include<map>
+
+#include "QNS.cpp"
+#include "Avenger.cpp"
+#include "Battle.cpp"
+
+int main()
+{
+    Battle B;
+    int k,n,m;      // k -> no of suits, n -> no of heroes, m -> no of enemies
+    std::cin >> k >> n >> m;
+    assert( k > 0 && n > 0 && m > 0 );
+    int p,d,e,h;
+    for (int i = 0 ; i < k ; i++ )
+    {
+        std::cin >> p >> d >> e >> h;
+        QNS suit(p,d,e,h);
+        B.addSuit(suit);
+    }
+    std::string name;
+    int attackstrength;
+    for (int i = 0 ; i < n + m ; i++ )
+    {
+        std::cin >> name >> attackstrength;
+        if (i >= k)
+        {
+            std::cout << name << " is out of fight \n";         // as no of suits are less 
+        }
+        else 
+        {   
+            Avenger avenger(name,B.getSuit(i),attackstrength);
+            if (i < n)
+                B.addHero(name);
+            else B.addEnemy(name);
+            B.addAvenger(name, avenger);
+        }
+    }
+    std::string s;
+    std::cin >> s;
+    if(s == "BattleBegin")
+        B.startBattle(); 
+}
